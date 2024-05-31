9eb6449ea72838e3accc3ff66535ec4e0a7d3f7f
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-05-31 19:22:36 -0400

    commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Had trouble installing pybaseball --> failed to build wheels for pyarrow. Did:
 $ pip3 install pyarrow==14.0.0
-$ python3 -m pip install -U pybaseball --user 
+$ python3 -m pip install -U pybaseball --user
+
+Python version 3.11.1
--- testing/spray_chart.py
+++ testing/spray_chart.py
@@ -5,6 +5,10 @@ from pybaseball import playerid_lookup
 import numpy as np
 import seaborn as sns
 
+firstName = 'Anthony'
+lastName = 'Volpe'
+start = '2023-01-01'
+stop = '2023-12-31'
 
 def getData(nameFirst, nameLast, startDate, endDate):
     player = playerid_lookup(nameLast, nameFirst)
@@ -19,3 +23,11 @@ def filterData(df):
     df["hit_or_out"] = (df.events == 'single') | (df.events == 'double') | (df.events == 'triple') | (df.events == 'home_run')
     return df
 
+def main():
+    data = getData(firstName, lastName, start, stop)
+    data = filterData(data)
+    print(data.head())
+
+
+if __name__ == "__main__":
+    main()
