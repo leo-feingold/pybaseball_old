94c8c3cd90ea9b08d8330878b079b3a74bec0b11
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-05-31 22:03:14 -0400

    commit

--- testing/test.py
+++ testing/test.py
@@ -4,29 +4,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 dateInitial = '2024-05-24'
 dateEnd = '2024-05-27'
 
 def scrapeAndSaveData(start, end):
     data = statcast(start_dt = start, end_dt = end)
-    data.to_csv(f"{start}_to_{end}_statcast_data.csv")
     return data
 
-def selectColumns(df):
-    columns = [
-    'pitch_type', 'game_date', 'release_speed', 'release_pos_x', 'release_pos_y', 
-    'release_pos_z', 'pitcher', 'stand', 'p_throws', 'pfx_x', 'pfx_z', 'plate_x', 'plate_z', 'vx0', 'vy0', 'vz0', 'ax', 'ay', 
-    'az', 'release_spin_rate', 'release_extension', 'spin_axis'
-    ]
-    df = df[columns]
-    return df
-
 def main():
-    data = scrapeAndSaveData(dateInitial, dateEnd)
-    data = selectColumns(data)
-    print(data.columns)
-
+    data = scrapeAndSaveData(start=dateInitial, end=dateEnd)
+    print(data.home_team)
 
 if __name__ == "__main__":
     main()
