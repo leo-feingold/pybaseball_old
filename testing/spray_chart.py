6e5b9a095227f62269d69f5d189bf73aa9c6d427
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-05-31 22:03:05 -0400

    commit

--- testing/spray_chart.py
+++ testing/spray_chart.py
@@ -7,8 +7,10 @@ import seaborn as sns
 from matplotlib.patches import Polygon
 
 
-firstName = 'Aaron'
-lastName = 'Judge'
+firstName = 'Anthony'
+lastName = 'Volpe'
+playerTeam = 'yankees'
+playerTeamTag = 'NYY'
 start = '2023-01-01'
 stop = '2023-12-31'
 
@@ -21,12 +23,22 @@ def getData(nameFirst, nameLast, startDate, endDate):
 def filterData(df):
     reg_season_condition = df["game_type"] == 'R'
     df = df.loc[reg_season_condition]
-    df = df[["hc_x", "hc_y", "events", "game_date", "launch_speed", "launch_angle"]]
+    df = df[["home_team", "hc_x", "hc_y", "events", "game_date", "launch_speed", "launch_angle"]]
     df["hit_or_out"] = (df.events == 'single') | (df.events == 'double') | (df.events == 'triple') | (df.events == 'home_run')
     df = df[df["hit_or_out"] == True]
+    df = df[df["home_team"] == playerTeamTag]
+
     return df
 
-def visualizeSprayChart(df):
+def plot_stadium(df, team):
+    fig, ax = plt.subplots()
+    stadium = pd.read_csv('/Users/leofeingold/Desktop/pybaseball/mlbstadiums.csv')
+    team_df = stadium[stadium['team'] == team.lower()]
+    for i in stadium['segment'].unique():
+        data = team_df[team_df['segment'] == i]
+        ax.plot(data['x'],-1*data['y'], linestyle = '-', color = 'black')
+    ax.axis('off')
+
     color_dict = {
         'single': 'brown',
         'double': 'green',
@@ -37,17 +49,16 @@ def visualizeSprayChart(df):
 
     for event, color in color_dict.items():
         subset = df[df['events'] == event]
-        plt.scatter(5*subset.hc_x, -5*subset.hc_y, color=color, label=event, alpha=0.7)
+        ax.scatter(subset.hc_x, -1*subset.hc_y, color=color, label=event, alpha=0.7)
 
-    plt.legend()
-    plt.title(f"{firstName} {lastName} Spray Chart ({start} - {stop})")
+    ax.legend()
+    ax.set_title(f"{firstName} {lastName} Spray Chart ({start} - {stop})")
     plt.show()
-
 
 def main():
     data = getData(firstName, lastName, start, stop)
     data = filterData(data)
-    visualizeSprayChart(data)
+    plot_stadium(data, playerTeam)
 
 if __name__ == "__main__":
     main()
