0228089727c18142420c1c4dadb4ae8499ca7aa9
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-06-03 12:37:51 -0400

    commit

--- stuffv1.py
+++ stuffv1.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
-dateInitial = '2024-05-26'
+dateInitial = '2024-05-20'
 dateEnd = '2024-05-27'
 
 def scrapeData(start, end):
@@ -14,12 +14,14 @@ def scrapeData(start, end):
     return data
 
 def cleanData(df): 
-    df = df[['pitch_type', 'release_speed', 
-        'release_pos_x', 'release_pos_z', 'spin_dir',
-        'pfx_x', 'pfx_z', 'plate_x', 'plate_z', 'vx0',
-        'vy0', 'vz0', 'ax', 'ay', 'az', 'release_spin_rate',
-        'release_extension', 'spin_axis' 'delta_run_exp']] 
-    df.dropna()
+    df.set_index('pitch_type', inplace=True)
+    df = df[['release_speed', 'release_pos_x', 
+        'release_pos_z','pfx_x', 
+        'pfx_z', 'release_spin_rate', 'release_extension',
+        'spin_axis', 'delta_run_exp']] 
+    # 'vx0','vy0', 'vz0', 'ax', 'ay', 'az', 'plate_x', 'plate_z', 
+    df = df.apply(pd.to_numeric, errors='coerce')
+    df = df.dropna()
     return df
 
 def splitData(df):
@@ -32,8 +34,7 @@ def splitData(df):
 def trainModel(X_train, X_test, y_train, y_test):
     model = LinearRegression()
     model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    return y_pred
+    return model
 
 def visualizeData(X_train, y_test, y_pred):
     r2 = r2_score(y_test, y_pred)
@@ -41,14 +42,22 @@ def visualizeData(X_train, y_test, y_pred):
     plt.scatter(y_test, y_pred)
     plt.xlabel("Actual Delta Run Expectancy")
     plt.ylabel("Predicted Delta Run Expectancy")
-    plt.title(f"Stuff+ Model: r^2 = {r^2}, ({numSamples} Samples)")
+    plt.title(f"Stuff+ Model: r^2 = {r2}, ({numSamples} Samples)")
     plt.show()
 
 def main():
     data = scrapeData(start=dateInitial, end=dateEnd)
+    print("Data scraped successfully.")
+    print(f"Scraped data shape: {data.shape}")
     data = cleanData(data)
+    print("Data cleaned successfully.")
+    print(f"Cleaned data shape: {data.shape}")
     X_train, X_test, y_train, y_test = splitData(data)
-    y_pred = trainModel(X_train, X_test, y_train, y_test)
+    print("Data split successfully.")
+    model = trainModel(X_train, X_test, y_train, y_test)
+    print("Model trained successfully")
+    y_pred = model.predict(X_test)
+    print("Prediction completed successfully.")
     visualizeData(X_train, y_test, y_pred)
 
 if __name__ == "__main__":
